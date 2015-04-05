2fc3d1dd76dfd4bfb6b59c60be76b0010ba0d700
mattizinho <matt.vieira12@hotmail.com>
2015-04-05 20:28:23 -0300

    Update Bully_temp.js

--- content/temp_json/Bully_temp.js
+++ content/temp_json/Bully_temp.js
@@ -5,13 +5,27 @@
         'company': ['Rockstar Games'],  
         'year': '2006',
         'date': '17-10-2006', // Day, Month, Year
-        'headline': 'Bully',
+        'headline': 'Bully - Scholarship Edition',
         'resume': [
-            'Lorem ipsum dolor sit amet, ut viverra vestibulum odio, ',
-            'aliquam in massa, velit rutrum pede ipsum mauris duis, ',
-            'congue massa sem elit, integer risus. Faucibus in lacus, ',
-            'ligula ornare nibh, elit suspendisse tortor sed volutpat, ',
-            'etiam tempus wisi semper montes odio, lorem vitae vel eu '
+            'Bully é um jogo eletrônico de ação-aventura em terceira pessoa, lançada ',
+            'pela Rockstar Vancouver para o Playstation 2 em 2006, onde o jogador ',
+            'entrará num mundo de intrigas, fofocas, lutas e entraves que todo ',
+            'colégio tem. "Bully" conseguiu capturar a essência do colegial em seu ',
+            'todo, não importa país, cultura ou língua. O jogo se passa na história ',
+            'de James Hopkins -Jimmy- em uma escola integral chamada Bullworth Academy, ',
+            'a escola mais louca de todo o país, pois foi a única que aceitou Jimmy  ',
+            'como aluno. O personagem é um rapaz muito mau, foi expulso de várias ',
+            'escolas por  Vandalismo, Linguagem imprópria, Graffiti, e Bullying. ',
+            'Como você está num internato, você terá de participar das aulas, que ',
+            'dão algum tipo de prémio, como, por exemplo, as aulas de inglês: Um ',
+            'vocabulário maior para se desculpar com os monitores e colegas que tentam ',
+            'agredi-lo. Química: ensinam a fazer bombinhas, bombas fedorentas, ',
+            'pó de mico, etc.; Bully, afinal, é um jogo divertido de se jogar por ',
+            'ser um jogo aberto e com várias coisas para fazer ao mesmo tempo. Dando'
+            'assim uma liberdade ao jogador, mas mesmo assim, dando um objetivo que'
+            'possa ser cumprido, seguindo a maneira Rockstar de criar seus jogos. '
+            'Um jogo violento mas divertido e um ótimo tradutor de tudo o que uma '
+            'escola pode oferecer, Bully é um jogo brilhante.'
         ].join(''),
         'full_text': [
             'Lorem ipsum dolor sit amet, ut viverra vestibulum odio, ',
